b3cc292f0f8fa2faf6015a8b070b3d6f39e5f68b
Jon Quach <hello@jonquach.com>
2019-03-11 17:11:04 -0400

    Update dev dependencies (#2)
    This update bumps Zero (Babel, Jest, ESLint) and React to the latest
    desired versions

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,7 +3,12 @@ module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     exclude: /__tests__/,
-    loader: require.resolve('awesome-typescript-loader'),
+    use: {
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: ['@helpscout/zero/babel'],
+      },
+    },
   })
   config.resolve.extensions.push('.ts', '.tsx')
   return config
